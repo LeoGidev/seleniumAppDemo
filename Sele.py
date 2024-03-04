66f55b5415e9afe7eb98226974be9be9818f7c2d
LeoGidev <leogimenez.dev@gmail.com>
2024-03-04 16:18:21 -0300

    se trabaja en estructura

---
+++ Sele.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from tkinter import Tk, Label, Text, Button, filedialog, Frame, ttk, Scale, Canvas
+import os
+from ttkthemes import ThemedTk
+import tkinter as tk
+from tkinter import ttk
